47412c8acd909c03fb30a7186bff2074bb298c3b
Mr WhiteKidd <austinslater58@gmail.com>
2022-11-09 18:22:57 -0600

    inital commit with starter code implemented

---
+++ StartScene.js
@@ -0,0 +1,18 @@
+class StartScene extends Phaser.Scene {
+    constructor() {
+      super({ key: 'StartScene' });
+    }
+  
+    preload() {
+      this.load.image('startScreen', 'https://content.codecademy.com/courses/learn-phaser/cube-matcher/start.png');
+    }
+  
+    create() {
+      screen = this.add.image(0, 0, 'startScreen').setOrigin(0);
+      this.input.on('pointerup', () => {
+        this.scene.stop('StartScene');
+        this.scene.start('GameScene');
+      });
+    }
+  }
+  
